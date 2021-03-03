d54fb3872ccfd7a129c9d9ca85d04efc433df5d9
Ted Won <jwon@redhat.com>
2021-03-03 22:24:46 +1000

    Added a feature for jar without pom.xml https://repo1.maven.org/maven2/com/google/inject/guice/4.0/guice-4.0.jar

--- src/main/java/io/alpaca/App.java
+++ src/main/java/io/alpaca/App.java
@@ -3,12 +3,14 @@ package io.alpaca;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 
+import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 /**
  * Alpaca is a Jar Analyzer project.
@@ -29,8 +31,8 @@ public class App {
             return;
         }
 
-        try (JarFile jar = new JarFile(Paths.get(jarPath).toFile())) {
-            Enumeration<JarEntry> entries = jar.entries();
+        try (JarFile jarFile = new JarFile(Paths.get(jarPath).toFile())) {
+            Enumeration<JarEntry> entries = jarFile.entries();
             while (entries.hasMoreElements()) {
                 JarEntry jarEntry = entries.nextElement();
                 String jarEntryName = jarEntry.getName();
@@ -38,32 +40,46 @@ public class App {
                     pomList.add(jarEntry);
                 }
 
-                // return only the jar file name
-                if (jarEntryName.endsWith(".jar")) {
+                // return only the jarFile file name
+                if (jarEntryName.endsWith(".jarFile")) {
                     jarList.add(jarEntry);
                 }
             }
 
             StringBuffer output = new StringBuffer();
-            for (int i = 0; i < pomList.size(); i++) {
-                final JarEntry jarEntry = pomList.get(i);
-                String jarEntryName = jarEntry.getName();
-                String jarVersion = "";
-                try {
-                    if (jarEntryName.matches("META-INF/maven/.*/pom.xml")) {
-                        Model model = new MavenXpp3Reader().read(jar.getInputStream(jarEntry));
-                        jarVersion = model.getVersion();
-                        if (jarVersion == null) {
-                            jarVersion = model.getParent().getVersion();
+            if (pomList.size() == 0) {
+                String jarName = new File(jarFile.getName()).getName();
+                output.append(jarName);
+//                Manifest manifest = jarFile.getManifest();
+//                if (manifest != null) {
+////                    Bundle-SymbolicName: com.google.inject
+////                    Bundle-Name: guice
+////                    Implementation-Title
+////                    Bundle-Version
+////                    Implementation-Version
+//                } else {
+//                }
+            } else {
+                for (int i = 0; i < pomList.size(); i++) {
+                    final JarEntry jarEntry = pomList.get(i);
+                    String jarEntryName = jarEntry.getName();
+                    String jarVersion = "";
+                    try {
+                        if (jarEntryName.matches("META-INF/maven/.*/pom.xml")) {
+                            Model model = new MavenXpp3Reader().read(jarFile.getInputStream(jarEntry));
+                            jarVersion = model.getVersion();
+                            if (jarVersion == null) {
+                                jarVersion = model.getParent().getVersion();
+                            }
                         }
+                        output.append(jarEntryName.replace("pom.xml", jarVersion));
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                    output.append(jarEntryName.replace("pom.xml", jarVersion));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
 
-                if (i != pomList.size() - 1) {
-                    output.append(",");
+                    if (i != pomList.size() - 1) {
+                        output.append(",");
+                    }
                 }
             }
 
@@ -75,7 +91,7 @@ public class App {
                 final JarEntry jarEntry = jarList.get(i);
                 String jarEntryName = jarEntry.getName();
 
-                // return only the jar file name
+                // return only the jarFile file name
                 final String[] split = jarEntryName.split("/");
                 if (split != null) {
                     jarEntryName = split[split.length - 1];
---
+++ src/test/java/io/alpaca/AppTest.java
@@ -0,0 +1,38 @@
+package io.alpaca;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
+/**
+ * https://repo1.maven.org/maven2/io/fabric8/kubernetes-openshift-uberjar/4.6.3/kubernetes-openshift-uberjar-4.6.3.jar
+ * <p/>
+ * https://get.jenkins.io/war-stable/2.263.4/jenkins.war
+ */
+public class AppTest {
+
+    @Test
+    @DisplayName("No pom.xml")
+    public void testPomZero() throws Exception {
+        URL resource = getClass().getClassLoader().getResource("guice-4.0.jar");
+        String absolutePath = Paths.get(resource.toURI()).toFile().getAbsolutePath();
+        String[] args = {absolutePath};
+        App.main(args);
+    }
+
+    @Test
+    @DisplayName("bundled jar")
+    public void testBundledJar() throws Exception {
+        String[] args = {"/tmp/jenkins.war"};
+        App.main(args);
+    }
+
+    @Test
+    @DisplayName("uber jar")
+    public void testUberJar() throws Exception {
+        String[] args = {"/tmp/kubernetes-openshift-uberjar-4.6.3.jar"};
+        App.main(args);
+    }
+}
